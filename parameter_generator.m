47561e1784ce3c69036d6b810006c7c871eeec10
sharknaedo <prasanna.maddila@student-cs.fr>
2022-01-04 19:19:35 +0100

    need to fix rulkov map

--- parameter_generator.m
+++ parameter_generator.m
@@ -1,7 +1,7 @@
 % Parameter file:
 % Taking the values from Table 3 in the paper.
 alpha = 0.1946; 
-gamma = 0.9241;
+gamma = 0.9251;
 delta = -0.1512;
 beta = 0.5770;
 mu = 0.4167;
@@ -11,7 +11,7 @@ n = 2;
 % Loading the time-series data from STLouis
 % and processing it for further use.
 stl_data = importdata('STLFSI2.csv') 
-stl_timestamps = stl_data.textdata(:, 1); 
+stl_timestamps = stl_data.textdata(2:end, 1); 
 stl_data = stl_data.data; 
 
 save('parameters.mat');
--- proj_finance.m
+++ proj_finance.m
@@ -4,22 +4,34 @@
 % Figure 3 and 4.
 
 %% System parameters and calls; 
-rng(0) % Seeds the random number generator
+% rng(0) % Seeds the random number generator
 clear all;  %#ok<CLALL>
 clc; 
 
 %% Initialising our Rulkov vectors
-t_max = 100; 
-x = zeros(t_max+1, 1); 
-y = zeros(t_max+1, 1); 
-
 % Parameters: 
 load('parameters'); 
 
+% Determined by the number of timestamps/data that we have. 
+t_max = length(stl_data); 
+
+x = zeros(t_max+1, 1); 
+y = zeros(t_max+1, 1); 
+
 % Initial Conditions 
-x(1) = 0; 
-y(1) = 0; 
+x(1) = stl_data(1); 
+y(1) = stl_data(1); 
 %% 
 % Modelling a Rulkov map needs functions 
-f = @(n, t, x) alpha ./ (1 + x(t).^n); 
+f = @(t, x) alpha ./ (1 + x(t).^n); 
+for t=1:t_max
+    x(t+1) = f(t, x) + gamma*y(t) + delta; 
+    disp(x(t+1))
+    y(t+1) = beta * y(t) - mu * x(t) + eta; 
+end
 
+hold on; 
+plot(y)
+plot(x)
+legend("Y", "X")
+grid on ; 
