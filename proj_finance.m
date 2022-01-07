2639e86aacaae49163771b955457dc9979af6d2b
sharknaedo <prasanna.maddila@student-cs.fr>
2022-01-07 23:30:26 +0100

    potentially fixed the model

--- proj_finance.m
+++ proj_finance.m
@@ -20,20 +20,17 @@ x = zeros(t_max+1, 1);
 % Initial Conditions 
 x(1) = stl_data(1);
 y(1) = stl_data(1);
-delta = -1 + (1+1)*rand(size(stl_data)); 
+%delta = ; 
 %% 
 % Modelling a Rulkov m% Determined by the number of timestamps/data that we have
-f = @(t, x) alpha ./ (1 + x(t).^n); 
-n = 20; 
-for t=1:t_max
-    x(t+1) = f(t, x) + gamma*y(t) + delta(t); 
-%x(t+1) = alpha / (1 + x(t)^n) + gamma*y(t) + delta; 
-%     disp(x(t+1))
-    y(t+1) = beta * y(t) - mu * x(t) + eta; 
+f = @(t, x) alpha ./ (1 + x(t).^n);  
+for t=1:t_max% plot(f(1:t_max,x))
+    x(t+1) = f(t, x) + gamma*y(t) + stl_data(t); %stl_data(t)=delta; 
+    y(t+1) = beta * y(t) - mu * x(t) + eta(); % eta*randn() for the second model
 end
 
 hold on; 
-plot(y); plot(stl_data); 
-% plot(f(1:t_max,x))
-legend("y","STL")
+plot(x);  
+plot(stl_data); 
+legend("x", "STL")
 grid on ; 
